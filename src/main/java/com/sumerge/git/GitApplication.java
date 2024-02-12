e55c4299935876714e0141cec84a6a775adaf513
ziaddakramm <106281067+ziaddakramm@users.noreply.github.com>
2024-02-12 13:57:00 +0200

    second commit-update main app

--- src/main/java/com/sumerge/git/GitApplication.java
+++ src/main/java/com/sumerge/git/GitApplication.java
@@ -2,8 +2,10 @@ package com.sumerge.git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GitApplication {
 
 	public static void main(String[] args) {
